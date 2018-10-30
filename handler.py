9b18da7527a073cb51575a2f0926e8f57bf0e1b5
Ivanq <imachug@yandex.ru>
2018-10-30 09:16:19 +0300

    Use spaces

--- handler.py
+++ handler.py
@@ -1,37 +1,37 @@
 from Site import SiteManager
 
 def handler(path, ip, port):
-	path = path.strip("/")
+    path = path.strip("/")
 
-	# Parse path.
-	# Defaults:
-	type = "1"
-	search = ""
-	if "/" in path:
-		# We have type and path
-		_, path = path.split("/", 1)
-	if "/" in path:
-		# We also have search string
-		path, search = path.split("/", 1)
+    # Parse path.
+    # Defaults:
+    type = "1"
+    search = ""
+    if "/" in path:
+        # We have type and path
+        _, path = path.split("/", 1)
+    if "/" in path:
+        # We also have search string
+        path, search = path.split("/", 1)
 
-	if search != "":
-		# Search isn't supported
-		yield "3", "Search is not supported yet."
-		return
+    if search != "":
+        # Search isn't supported
+        yield "3", "Search is not supported yet."
+        return
 
-	# Handle home page
-	if path == "":
-		yield "i", "Welcome to ZeroNet Gopher proxy!"
-		yield "i", "Site list follows:"
-		yield
+    # Handle home page
+    if path == "":
+        yield "i", "Welcome to ZeroNet Gopher proxy!"
+        yield "i", "Site list follows:"
+        yield
 
-		for address, site in SiteManager.site_manager.sites.iteritems():
-			# Try to get site title
-			try:
-				content_json = site.content_manager.contents["content.json"]
-				title = content_json["title"]
-			except:
-				# Fallback to address
-				title = address
+        for address, site in SiteManager.site_manager.sites.iteritems():
+            # Try to get site title
+            try:
+                content_json = site.content_manager.contents["content.json"]
+                title = content_json["title"]
+            except:
+                # Fallback to address
+                title = address
 
-			yield "1", title, "/" + address, ip, port
+            yield "1", title, "/" + address, ip, port
