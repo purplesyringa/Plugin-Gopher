b386394b47e5ccddd4bc948c12a7eb589c08eb17
Ivanq <imachug@yandex.ru>
2018-11-02 21:52:29 +0300

    Support clearnet

--- HTTPGopherProxy.py
+++ HTTPGopherProxy.py
@@ -16,21 +16,30 @@ def format(text, path, ip, port):
         title = title.replace("    ", "&nbsp;&nbsp;&nbsp;&nbsp;").replace("<", "&lt;").replace(">", "&gt;").replace("\\", "\\\\")
 
         location = urllib.quote(parts[1]) if len(parts) >= 2 else ""
-        host = parts[2] if len(parts) >= 3 else ip
-        port = parts[3] if len(parts) >= 4 else port
+
+        if path.startswith("gopher://"):
+            # Proxy
+            host = parts[2] if len(parts) >= 3 else "(null.host)"
+            port = parts[3] if len(parts) >= 4 else 70
+            location = "/gopher://%s:%s/%s%s" % (host, port, gophertype, location)
+        else:
+            # Local
+            host = parts[2] if len(parts) >= 3 else ip
+            port = parts[3] if len(parts) >= 4 else port
+            location = gophertype + location
 
         if gophertype == "i":
             gopher_text += u"%s<br>\n" % title
         elif gophertype == "3":
             gopher_text += u"<strong>ERR</strong> <em style='color: red'>%s</em><br>\n" % title
         elif gophertype == "1":
-            if location.startswith("URL:") or location.startswith("GET "):
+            if location.startswith("URL%3A"):
                 gopher_text += u"<img src='/I/gophermedia/web.png'> <a href='%s'>%s</a> &lt;WEB&gt;<br>\n" % (location[4:], title)
             else:
-                gopher_text += u"<img src='/I/gophermedia/dir.png'> <a href='//%s:%s/1%s'>%s/</a><br>\n" % (host, port, location, title)
+                gopher_text += u"<img src='/I/gophermedia/dir.png'> <a href='%s'>%s/</a><br>\n" % (location, title)
         elif gophertype == "7":
             gopher_text += u"<img src='/I/gophermedia/inp.png'> %s &lt;INP&gt;<br>\n" % title
-            gopher_text += u"<form action='//%s:%s/7%s'>" % (host, port, location)
+            gopher_text += u"<form action='%s'>" % location
             gopher_text += u"<img src='/I/gophermedia/blank.png'>"
             gopher_text += u"<img src='/I/gophermedia/inp2.png'> "
             gopher_text += u"<input type='text' id='search_%s' name='search'>" % title.replace(" ", "_")
@@ -49,9 +58,9 @@ def format(text, path, ip, port):
                 "T": "3270",
                 "s": "SND"
             }[gophertype]
-            gopher_text += u"<img src='/I/gophermedia/%s.png'> <a href='//%s:%s/%s%s'>%s</a> &lt;%s&gt;<br>\n" % (desc.lower(), host, port, gophertype, location, title, desc)
+            gopher_text += u"<img src='/I/gophermedia/%s.png'> <a href='%s'>%s</a> &lt;%s&gt;<br>\n" % (desc.lower(), location, title, desc)
         elif gophertype == "h":
-            gopher_text += u"<img src='/I/gophermedia/html.png'> <a href='//%s:%s/h%s'>%s</a> &lt;HTML&gt; <strong>(No sandbox)</strong><br>\n" % (host, port, location, title)
+            gopher_text += u"<img src='/I/gophermedia/html.png'> <a href='%s'>%s</a> &lt;HTML&gt; <strong>(No sandbox)</strong><br>\n" % (location, title)
         else:
             gopher_text += u"%s<br>\n" % line
 
